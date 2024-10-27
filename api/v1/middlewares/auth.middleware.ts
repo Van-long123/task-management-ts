0a864c5cc08787406fa7de4e0ca7f545b0088f34
unknown <phamlong123np@gmail.com>
2024-10-27 22:37:05 +0700

    Chuyển các route thành privateprivate

--- api/v1/middlewares/auth.middleware.ts
+++ api/v1/middlewares/auth.middleware.ts
@@ -2,7 +2,8 @@ import User from "../models/user.model"
 import {Request,Response,NextFunction} from 'express';
 // nhiệm vụ middleware này kiểm tra xem ở trong header của ông fe đưa lên có chứa token ko 
 // khi viết theo kiểu api thì fe luôn gửi token lên khi api này là private 
-export const requireAuth=async (req:Request, res:Response,next:NextFunction)=>{
+// next ()ko trả về gì cả dùng void mà async là hàm bất đồng bộ them promise
+export const requireAuth=async (req:Request, res:Response,next:NextFunction):Promise<void>=>{
     if(req.headers.authorization){
         const token=req.headers.authorization.split(' ')[1];
         const user=await User.findOne({
@@ -16,6 +17,9 @@ export const requireAuth=async (req:Request, res:Response,next:NextFunction)=>{
             })
             return 
         }
+
+        // ở req:Request thì bắt buộc phải tuân theo kiểu   req['user'] này chớ ko phải req.user 
+        
         req['user']=user 
         next()
         
